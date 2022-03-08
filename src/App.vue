c06bb4712bba00e253d7773e5ca5e593dd5842ad
Karlo Siladi <karlo.siladi@inteligencija.com>
2022-03-08 10:36:28 +0100

    Initial commit - starter

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+ <div class="mx-auto max-w-screen-xl">
+    <div style="grid-template-columns: 1fr 2fr 3fr;" class="grid gap-4 m-4">
+      <img
+        src="http://valleygrowlers.com/wp-content/uploads/2013/11/clean_line_circle_logo-300x300.jpg"
+      />
+      <div
+        class="text-2xl"
+      >
+        Come join us every day starting at noon… Valley Growlers would love to have you as a guest. Come have a pint or take a growler or crowler to go....
+      </div>
+      <div id="search">
+        <div
+          class="p-3 border-blue-300 border-8 border-dashed text-3xl font-bold text-center"
+        >
+          Search Micro-FE
+        </div>
+      </div>
+    </div>
+    <div style="grid-template-columns: 3fr 1fr;" class="grid gap-4 m-4">
+      <div>
+        <img
+          src="http://valleygrowlers.com/wp-content/uploads/2013/11/ValleyGrowlers12.png"
+          style="object-fit: cover; width: 100%; max-height: 200px;"
+        />
+        <div class="m-3 text-xl">
+          We also like <span id="like" class="font-bold"></span>.
+        </div>
+        <div id="taps">
+          <div
+            class="p-3 border-blue-300 border-8 border-dashed text-3xl font-bold text-center"
+          >
+            Tap List Micro-FE
+          </div>
+        </div>
+      </div>
+      <div>
+        <div
+          class="font-bold text-2xl mb-3"
+        >
+          What's In Your Growler
+        </div>
+        <div id="cart">
+          <div
+            class="p-3 border-blue-300 border-8 border-dashed text-3xl font-bold text-center"
+          >
+            Cart Micro-FE
+          </div>
+        </div>
+        <div
+          class="mt-3"
+        >
+          We have growlers and cans available if you don't have them already.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({});
+</script>
